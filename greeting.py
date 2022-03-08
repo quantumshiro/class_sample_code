38053708860d1634cff8a19e19791d1d65a85635
quantumshiro <hiro.1nakanishi@gmail.com>
2022-03-08 22:46:58 +0900

    sample file

---
+++ greeting.py
@@ -0,0 +1,22 @@
+class Greeting:
+    def __init__(self, name):
+        self.ownanme = name
+
+    def english(self):
+        print("Hello, " + self.ownanme)
+    
+    def espanol(self):
+        print("Hola, " + self.ownanme)
+    
+    def japanese(self):
+        print("Konnichiwa, " + self.ownanme)
+
+def main():
+    name = input("What is your name? ")
+    greeting = Greeting(name)
+    greeting.english()
+    greeting.espanol()
+
+
+if __name__ == "__main__":
+    main()
